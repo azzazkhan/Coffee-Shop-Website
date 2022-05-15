53b479a6081a50bfb53c84638c556992e604ba2e
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-05-16 00:12:57 +0500

    Coffee Shop Website

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const hbs = require("hbs");
+const path = require("path");
+
+const port = process.env.PORT || 8000;
+
+// Setting static path
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Setting view engine
+app.set("view engine", "hbs");
+
+// Setting views directory path
+app.set("views", path.join(__dirname, "../templates/views"));
+
+// Registering partials.
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+// Routing
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
