1b97d3215348eec738e48f716f52caeddc7db4d5
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-05-17 11:14:19 +0500

    slider added

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,30 @@
 let image_slider_next_btn = document.getElementById("image_slider_next_btn");
-let coffee_slider_images = document.getElementById("coffee_slider_images");
+var coffee_slider_images = document.querySelector(".coffee_slider_images");
 
-image_slider_next_btn.addEventListener("click", () => {
-    coffee_slider_images.style.transform = `translateX(-10%)`;
+
+
+let img_slider_event_listener = () => {
     let coffee_slider_img_container = document.getElementsByClassName("coffee_slider_img_container");
-
-    for(elements of coffee_slider_img_container){
-        if(elements.classList.contains("coffee_slider_img_container_selected")){
-            elements.classList.remove("coffee_slider_img_container_selected");
-            elements.nextElementSibling.classList.add("coffee_slider_img_container_selected");
+    let arr = Array.from(coffee_slider_img_container);
+    coffee_slider_images.style.transform += `translateX(-10%)`;
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i].classList.contains("coffee_slider_img_container_selected")){
+            if(arr.indexOf(arr[i]) == 9){
+                coffee_slider_images.style.transform = `translateX(0%)`;
+                arr[i].classList.remove("coffee_slider_img_container_selected");
+                arr[i].children[0].classList.remove("img_5");
+                i = 0;
+            }
+            console.log(arr.indexOf(arr[i]));
+            arr[i].classList.remove("coffee_slider_img_container_selected");
+            // div.firstChild.classList.remove("img_5");
+            arr[i].children[0].classList.remove("img_5");
+            arr[i].nextElementSibling.classList.add("coffee_slider_img_container_selected");
+            arr[i].nextElementSibling.children[0].classList.add("img_5");
+            break;
         }
     }
-});
+};
+
+image_slider_next_btn.addEventListener("click", img_slider_event_listener);
