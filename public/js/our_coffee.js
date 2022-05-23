2471d9fced36631905b98c0ff3f82095ae7b2704
Muhammad Tayyab <muhammadtayyab3411@gmail.com>
2022-05-23 23:51:51 +0500

    our coffee page added

--- public/js/our_coffee.js
+++ public/js/our_coffee.js
@@ -23,3 +23,53 @@ intro_section_paragraph.innerText = intro_section_paragraph_Text;
 
 let root = document.querySelector(":root");
 root.style.setProperty("--intro_section_bg_img_url", `url("../images/kava14316_kkedit_desktop_1.jpg")`);
+
+const inner_navigation_section = document.querySelectorAll(".inner_navigation_section");
+const our_coffee_navigation_item = document.querySelectorAll(".our_coffee_navigation_item");
+
+
+window.addEventListener("scroll", () => {
+    let currentSection = ``;
+
+    inner_navigation_section.forEach(section => {
+        if(window.scrollY >= (section.offsetTop - section.clientHeight / 3)){
+            currentSection = section.getAttribute("class");
+        }
+    });
+    
+    if(currentSection.includes("coffee_address")){
+        our_coffee_navigation_item.forEach(item => {
+            item.classList.remove("item_active");
+            if(item.classList.contains("coffee_address")){
+                item.classList.add("item_active");
+            }
+        });
+    }
+    
+    else if(currentSection.includes("atlas")){
+        our_coffee_navigation_item.forEach(item => {
+            item.classList.remove("item_active");
+            if(item.classList.contains("atlas")){
+                item.classList.add("item_active");
+            }
+        });
+    }
+
+    else if(currentSection.includes("lavazza")){
+        our_coffee_navigation_item.forEach(item => {
+            item.classList.remove("item_active");
+            if(item.classList.contains("lavazza")){
+                item.classList.add("item_active");
+            }
+        });
+    }
+
+    else if(currentSection.includes("kimbo")){
+        our_coffee_navigation_item.forEach(item => {
+            item.classList.remove("item_active");
+            if(item.classList.contains("kimbo")){
+                item.classList.add("item_active");
+            }
+        });
+    }
+});
